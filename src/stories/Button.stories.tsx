1654e3c21f88dfd36525a093461cb423985a8bf2
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2023-02-24 21:21:18 +0900

    feat : storybook button default/hover 추가

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,36 +1,36 @@
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Button } from "./Button";
+
 export default {
   title: "Example/Button",
   component: Button,
-  argTypes: {
-    backgroundColor: { control: "color" },
+};
+
+const Template = (args: any) => <Button {...args}>Label</Button>;
+
+export const Default = Template.bind({});
+
+export const Hover: any = Template.bind({});
+Hover.parameters = { pseudo: { hover: true } };
+
+export const DirectSelector = () => (
+  <>
+    <div className="story-grid">
+      <Button>Normal</Button>
+      <Button data-hover>Hover</Button>
+    </div>
+    <h3>Multiple hovered button grouped</h3>
+    <div data-hover-group>
+      <Button>Hovered 1</Button>
+      <Button>Hovered 2</Button>
+      <Button>Hovered 3</Button>
+    </div>
+  </>
+);
+
+DirectSelector.parameters = {
+  pseudo: {
+    hover: ["[data-hover]", "[data-hover-group] button"],
   },
-} as ComponentMeta<typeof Button>;
-
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
-  label: "Button",
 };
