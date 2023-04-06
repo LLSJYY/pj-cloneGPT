398a7329120e1a0d4577ec9c069e90a82e433a5c
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2023-04-06 21:38:57 +0900

    chore : storybook parameter backgrounds 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
+import { Preview } from "@storybook/react";
 export const parameters = {
+  backgrounds: {
+    default: "dark",
+    values: [
+      {
+        name: "dark",
+        value: "#202123",
+      },
+      {
+        name: "normal",
+        value: "#ffffff",
+      },
+    ],
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
